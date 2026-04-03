48a1783d15eebfb97cd368ecb828e55580075942
Trevor Ewart <tmwai@kabarak.ac.ke>
2026-04-03 15:01:05 +0300

    initial MERN setup with homepage in progress

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favorite food here</h2>
+        <p>Craving something tasty? Explore great food near you and enjoy quick, hassle-free delivery.</p>
+        <button>Explore Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
